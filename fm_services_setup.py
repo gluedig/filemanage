5215146edd5b770d3b87b58add14be7f3fd33fbe
Piotr Haber <gluedig@gmail.com>
2013-08-07 20:59:27 +0200

    update setup script

--- fm_services_setup.py
+++ fm_services_setup.py
@@ -3,13 +3,13 @@ Created on Jun 20, 2013
 
 @author: developer
 '''
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='Sensus FM services',
     version='0.1',
     long_description=__doc__,
-    packages=['fm_services'],
+    packages=find_packages(exclude=['mac_resolver', 'proximity_mon']),
     include_package_data=True,
     zip_safe=False,
     install_requires=['Flask', 'blinker'],
