fe6d9f9dd8cfeb968cabd1c6bb493e266acec864
Piotr Haber <gluedig@gmail.com>
2013-09-19 18:41:58 +0200

    remove test tornado handler

--- run_services.py
+++ run_services.py
@@ -7,14 +7,10 @@ from fm_services import app
 from fm_services.events_sockjs import HubEventsGetHandler
 from tornado.wsgi import WSGIContainer
 from tornado.ioloop import IOLoop
-from tornado.web import FallbackHandler, RequestHandler, Application
+from tornado.web import FallbackHandler, Application
 from tornado.options import parse_command_line
 from tornado.log import app_log
 import sys
-
-class MainHandler(RequestHandler):
-    def get(self):
-        self.write("This message comes from Tornado ^_^")
 
 tr = WSGIContainer(app)
 
@@ -22,7 +18,7 @@ handlers = [
             (r"/events/hub/(.+)", HubEventsGetHandler),
             (r".*", FallbackHandler, dict(fallback=tr)),
         ]
-application = Application(handlers, debug=True)
+application = Application(handlers)
 
 if __name__ == "__main__":
     parse_command_line(sys.argv)
