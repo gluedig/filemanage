5f602d86c833832b65a876e2b2952bbc7c7f3b88
Piotr Haber <gluedig@gmail.com>
2013-09-19 14:27:23 +0200

    logging through tornado

--- run_services.py
+++ run_services.py
@@ -7,6 +7,9 @@ from fm_services import app
 from tornado.wsgi import WSGIContainer
 from tornado.ioloop import IOLoop
 from tornado.web import FallbackHandler, RequestHandler, Application
+from tornado.options import parse_command_line
+from tornado.log import app_log
+import sys
 
 class MainHandler(RequestHandler):
     def get(self):
@@ -20,6 +23,7 @@ application = Application([
 ])
 
 if __name__ == "__main__":
-    print("Running on port %i"%app.config['LISTEN_PORT'])
+    parse_command_line(sys.argv)
+    app_log.info("Running on port %i"%app.config['LISTEN_PORT'])
     application.listen(app.config['LISTEN_PORT'])
     IOLoop.instance().start()
