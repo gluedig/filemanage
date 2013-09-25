3c4841a8266df8e7c56979fc80a3a08b0f36346c
Piotr Haber <gluedig@gmail.com>
2013-09-25 14:03:41 +0200

    update dependencies

--- fm_services_setup.py
+++ fm_services_setup.py
@@ -7,11 +7,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='Sensus FM services',
-    version='0.1',
+    version='0.2',
     long_description=__doc__,
     packages=find_packages(exclude=['mac_resolver', 'proximity_mon']),
     include_package_data=True,
     zip_safe=False,
-    install_requires=['Flask', 'blinker'],
+    install_requires=['Flask', 'blinker', 'SQLAlchemy', 'sockjs-tornado' ],
     package_data={'fm_services': ['static/*', 'templates/*']}
 )
